34a881c9c71a8a121e34fff8c7afb177158baa92
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-02-21 13:14:58 +0900

    모델 활성화상태 거짓디폴트

--- backend/accountapp/models.py
+++ backend/accountapp/models.py
@@ -44,7 +44,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=20, null=False,
                                 unique=True, validators=[username_pattern])
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
