4817ec64fb63e715f641619f40eff19792b4bde4
곽병혁 <aiden-kwak@gwagbyeonghyeog-ui-MacBookPro.local>
2024-02-22 00:04:09 +0900

    chat ux updated, --vh

--- frontend/src/chat/ChatForm.jsx
+++ frontend/src/chat/ChatForm.jsx
@@ -34,6 +34,20 @@ function ChatForm() {
     const WS_BASE_URL = URLManagement('ws');
 
     useEffect(() => {
+        function setScreenSize() {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+        setScreenSize(); // 페이지가 로드될 때 한 번 호출
+        // 브라우저 창의 크기가 변경될 때마다 호출
+        window.addEventListener('resize', setScreenSize);
+        // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+        return () => {
+            window.removeEventListener('resize', setScreenSize);
+        };
+    }, []);
+
+    useEffect(() => {
         // 로그인되지 않은 경우 로그인 페이지로 리디렉트
         if (!user) {
             navigate('/login');
